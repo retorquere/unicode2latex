7ee09df88e9dd18086d671182beeec2e06351070
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-02-20 14:33:23 +0100

    index file

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ascii: {
+    math: require('./ascii/math.json'),
+    text: require('./ascii/text.json'),
+  },
+  unicode: {
+    math: require('./unicode/math.json'),
+    text: require('./unicode/text.json'),
+  },
+};
