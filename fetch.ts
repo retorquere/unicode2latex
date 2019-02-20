a7e9b421a67c95ecce1f4b1d017505856ca79549
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-02-20 22:58:25 +0100

    non-space spaces -> latex

--- fetch.ts
+++ fetch.ts
@@ -56,7 +56,7 @@ function escapedJSON(obj) {
 class CharMap {
   private source: string
   private charmap: { [key: string]: { tex: string, mode: 'text' | 'math', source: string, target: 'unicode' | 'ascii' } }
-  private always_replace = '\u00A0<>\\#$%&^_{}~'
+  private always_replace = '\u00A0\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u200B\u202F\u205F\u3000\uFEFF<>\\#$%&^_{}~'
 
   constructor() {
     this.source = ''
