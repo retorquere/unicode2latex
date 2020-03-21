fd18d376d4fab7ce2cec82c79b3d880b10ce790f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-03-22 00:05:04 +0100

    typings

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 export declare const ascii: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
 export declare const unicode: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
 export declare const latex: Record<string, string>
+export declare const diacritics: {
+  commands: string[],
+  tolatex: Record<string, string>
+  tounicode: Record<string, string>
+}
--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   ascii: require('./tables/ascii.json'),
   unicode: require('./tables/unicode.json'),
   latex: require('./tables/latex.json'),
+  diacritics: require('./tables/diacritics.json'),
 };
