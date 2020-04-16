e0f083bcda40236b9af888486208a33ded587ea1
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-04-16 16:42:36 +0200

    typings

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 export declare const ascii: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
+export declare const ascii_bibtex_creator: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
 export declare const unicode: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
 export declare const latex: Record<string, string>
 export declare const diacritics: {
