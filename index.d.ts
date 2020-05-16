78c4b74d89a5d8a7836c4ad4bc51e6b6a6951293
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-05-16 12:09:38 +0200

    commandspacer

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-export declare const ascii: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
-export declare const ascii_bibtex_creator: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
-export declare const unicode: Record<string, { text?: string; math?: string; space?: boolean; packages: string[]; }>
+export declare const ascii: Record<string, { text?: string; math?: string; space?: boolean; commandspacer?: boolean, packages: string[]; }>
+export declare const ascii_bibtex_creator: Record<string, { text?: string; math?: string; space?: boolean; commandspacer?: boolean, packages: string[]; }>
+export declare const unicode: Record<string, { text?: string; math?: string; space?: boolean; commandspacer?: boolean, packages: string[]; }>
 export declare const latex: Record<string, string>
 export declare const diacritics: {
   commands: string[],
