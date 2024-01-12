79f596e85ad8c9e1ffbfca205a71af4b85fa10a8
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-01-12 16:44:57 +0100

    2latex

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ export const diacritics: {
   tounicode: Record<string, string>
 } = require('./tables/diacritics.json')
 
-export type Options = {
+export type MapOptions = {
   packages?: string[]
   math?: string
   text?: string
@@ -29,7 +29,7 @@ export type Options = {
   charmap?: CharMap
 }
 
-export function load(mode : 'bibtex' | 'biblatex' | 'minimal',  options?: Options): CharMap {
+export function load(mode : 'bibtex' | 'biblatex' | 'minimal',  options?: MapOptions): CharMap {
   let map = { ...maps[mode].base }
   const packages = maps[mode].package
   for (const pkg of (options.packages || []).map(p => packages[p]).filter(p => p)) {
@@ -47,4 +47,139 @@ export function load(mode : 'bibtex' | 'biblatex' | 'minimal',  options?: Option
 
   if (options.charmap) map = { ...map, ...options.charmap }
   return map
+}
+
+export function replace_command_spacers(latex: string): string {
+  return latex.replace(/\0(\s)/g, '{}$1').replace(/\0([^;.,!?${}_^\\/])/g, ' $1').replace(/\0/g, '')
+}
+
+const switchMode = {
+  math: 'text',
+  text: 'math',
+}
+const re = /([^\u0300-\u036F][\u0300-\u036F]+)|(i\uFE20a\uFE21)|([\uD800-\uDBFF][\uDC00-\uDFFF])|([\uD800-\uDFFF])|./g
+export type TranslateOptions = {
+  bracemath?: boolean
+  mode?: 'minimal' | 'bibtex' | 'biblatex'
+  preservecommandspacers?: boolean
+  packages?: Set<string>
+}
+export function tolatex(text: string, table: CharMap, options: TranslateOptions = {}) {
+  options = { bracemath: false, mode: 'biblatex', preservecommandspacers: false, packages: new Set, ...options}
+  let mode = 'text'
+  let braced = 0
+
+  const switchTo = {
+    math: (options.bracemath ? '{$' : '$'),
+    text: (options.bracemath ? '$}' : '$'),
+  }
+
+  let mapped // : TeXChar
+  let switched // : boolean
+  let m // : RegExpExecArray | RegExpMatchArray
+  let cd // : { command: string, mode: string }
+
+  let latex = ''
+  text.replace(re, (match: string, cdpair: string, tie: string, pair: string, single: string) => {
+    mapped = null
+    if (tie && !table[tie]) {
+      mapped = { text: 'ia' }
+    }
+    else {
+      // does that last one do anything?
+      mapped = table[tie] || table[cdpair] || table[pair] || table[single] || (cdpair && table[cdpair.normalize('NFC')])
+    }
+
+    if (!mapped && options.mode !== 'minimal' && cdpair && (cd = diacritics.tolatex[cdpair.substr(1)])) {
+      let char
+      if (char = (table[cdpair[0]] || { text: cdpair[0], math: cdpair[0] })[cd.mode]) {
+        const cmd = cd.command.match(/[a-z]/)
+
+        if (options.mode === 'bibtex' && cd.mode === 'text') {
+          // needs to be braced to count as a single char for name abbreviation
+          mapped = ({ [cd.mode]: `{\\${cd.command}${cmd ? ' ': ''}${char}}` } /*as TeXChar*/)
+        }
+        else if (cmd && char.length === 1) {
+          mapped = ({ [cd.mode]: `\\${cd.command} ${char}` } /*as TeXChar*/)
+        }
+        else if (cmd) {
+          mapped = ({ [cd.mode]: `\\${cd.command}{${char}}` } /*as TeXChar*/)
+        }
+        else {
+          mapped = ({ [cd.mode]: `\\${cd.command}${char}` } /*as TeXChar*/)
+        }
+      }
+    }
+
+    /* ??
+    if (!mapped && text[i + 1] && (mapped = this.mapping[text.substr(i, 2)])) {
+      i += 1
+    }
+    */
+    // fallback -- single char mapping
+    if (!mapped) mapped = { text: match }
+
+    // in and out of math mode
+    if (!mapped[mode]) {
+      mode = switchMode[mode]
+      latex += switchTo[mode]
+      switched = true
+    }
+    else {
+      switched = false
+    }
+
+    // balance out braces with invisible braces until
+    // http://tex.stackexchange.com/questions/230750/open-brace-in-bibtex-fields/230754#comment545453_230754
+    // is widely deployed
+    switch (mapped[mode]) {
+      case '\\{': braced += 1; break
+      case '\\}': braced -= 1; break
+    }
+    if (braced < 0) {
+      latex += '\\vphantom\\{'
+      braced = 0
+    }
+
+    // if we just switched out of math mode, and there's a lone sup/sub at the end, unpack it.
+    // The extra option brace is for when we're not in bracemath mode (see switchTo)
+    if (switched && mode === 'text' && (m = latex.match(/([\^_])\{(.)\}(\$\}?)$/))) {
+      latex = latex.slice(0, latex.length - m[0].length) + m[1] + m[2] + m[3]
+    }
+
+    latex += mapped[mode]
+    if (mapped.commandspacer) latex += '\0' // clean up below
+
+    // only try to merge sup/sub if we were already in math mode, because if we were previously in text mode, testing for _^ is tricky.
+    if (!switched && mode === 'math' && (m = latex.match(/(([\^_])\{[^{}]+)\}\2{(.\})$/))) {
+      latex = latex.slice(0, latex.length - m[0].length) + m[1] + m[3]
+    }
+
+    if (mapped.alt) {
+      for (const pkg of mapped.alt) {
+        options.packages.add(pkg)
+      }
+    }
+    return match // pacify tsc
+  })
+
+  // add any missing closing phantom braces
+  switch (braced) {
+    case 0:
+      break
+    case 1:
+      latex += '\\vphantom\\}'
+      break
+    default:
+      latex += `\\vphantom{${'\\}'.repeat(braced)}}`
+      break
+  }
+
+  // might still be in math mode at the end
+  if (mode === 'math') latex += switchTo.text
+
+  // does this do anything?
+  // latex = latex.normalize('NFC')
+  if (!options.preservecommandspacers) replace_command_spacers(latex)
+  return latex
 }
--- translate.js
+++ translate.js
@@ -7,24 +7,9 @@ const nfd = sample.normalize('NFD')
 
 console.log(sample === nfd)
 
-const table = u2l.biblatex.base
-
-const re = /([\uD800-\uDBFF][\uDC00-\uDFFF])|([\uD800-\uDFFF])|./g
-function latex(s, packages) {
-  return s.replace(re, (char, pair, single) => {
-    const mapping = table[pair] || table[single] || {}
-    if (mapping.alt) {
-      for (const pkg of mapping.alt) {
-        packages.add(pkg)
-      }
-    }
-    const $ = mapping.math ? '$' : ''
-    return $ + (mapping.text || mapping.math || char) + $
-  })
-}
-
 for (let word of process.argv.slice(2).concat(sample)) {
-  let packages = new Set
-  console.log(latex(word, packages))
+  const packages = new Set
+  console.log(u2l.tolatex(word, u2l.biblatex.base, { packages }))
   console.log('packages:', [...packages])
 }
+
