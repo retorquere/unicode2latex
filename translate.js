b3f2c41bdad4b888abe8888ee94cf49c938f74b5
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-01-12 14:25:42 +0100

    double-byte mappings

--- translate.js
+++ translate.js
@@ -2,27 +2,29 @@
 
 const u2l = require('./index')
 
-// const nfc = "⩽".normalize('NFC')
-const nfc = "§".normalize('NFC')
-const nfd = nfc.normalize('NFD')
+const sample = "Matrices with Small Coherence Using 𝑝-Ary Block Codes".normalize('NFC')
+const nfd = sample.normalize('NFD')
 
-// const table = u2l.biblatex.package.amssymb
+console.log(sample === nfd)
+
 const table = u2l.biblatex.base
 
-const re = /([\uD800-\uDBFF][\uDC00-\uDFFF])|./g
-function texify(char, pair) {
-  const mapping = table[pair] || table[char] || {}
-  return mapping.text || mapping.math || char
-}
-function latex(s) {
-  return s.replace(re, texify)
+const re = /([\uD800-\uDBFF][\uDC00-\uDFFF])|([\uD800-\uDFFF])|./g
+function latex(s, packages) {
+  return s.replace(re, (char, pair, single) => {
+    const mapping = table[pair] || table[single] || {}
+    if (mapping.alt) {
+      for (const pkg of mapping.alt) {
+        packages.add(pkg)
+      }
+    }
+    const $ = mapping.math ? '$' : ''
+    return $ + (mapping.text || mapping.math || char) + $
+  })
 }
 
-console.log(latex(nfc))
-console.log(latex(nfd))
-
-for (const word of process.argv.slice(2).concat(nfc)) {
-  let packages = []
-  console.log(latex(word))
-  console.log('packages:', [...(new Set(packages))])
+for (let word of process.argv.slice(2).concat(sample)) {
+  let packages = new Set
+  console.log(latex(word, packages))
+  console.log('packages:', [...packages])
 }
